7a907d4964305cd721dfffec598720c1c37fc9a7
Kateryna Skoryna <k.skoryna@gmail.com>
2023-01-15 23:00:20 +0100

    Create schema updateUser

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,17 +9,27 @@ const {
   users: { signup, login, getCurrent, logout, updateUser, updateAvatar },
 } = require("../../controllers");
 
-const { signupUserSchema, loginUserSchema } = require("../../schemas");
+const {
+  signupUserSchema,
+  loginUserSchema,
+  updateUserSchema,
+} = require("../../schemas");
 const router = express.Router();
 
 const userSignupValidation = validation(signupUserSchema);
 const userLoginValidation = validation(loginUserSchema);
+const userUpdateValidation = validation(updateUserSchema);
 
 router.post("/signup", userSignupValidation, controllerlWrapper(signup));
 router.post("/login", userLoginValidation, controllerlWrapper(login));
 router.get("/current", auth, controllerlWrapper(getCurrent));
 router.post("/logout", auth, controllerlWrapper(logout));
-router.patch("/users", auth, controllerlWrapper(updateUser));
+router.patch(
+  "/users",
+  auth,
+  userUpdateValidation,
+  controllerlWrapper(updateUser)
+);
 router.patch(
   "/avatars",
   auth,
